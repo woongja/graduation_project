c33400dc531c0917150d12e6280ad6575450d40f
woongja <wcn4457@gmail.com>
2023-07-13 17:55:04 +0900

    7.13

--- lib/history_page.dart
+++ lib/history_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:page_transition/page_transition.dart';
 import 'package:photo_app/FirstPage.dart';
 
@@ -10,25 +11,46 @@ class HistoryPage extends StatelessWidget {
     return MaterialApp(
       title: 'history_page',
       home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.black87,
-          elevation: 5,
-          centerTitle: true,
-          title: Text(
-            'history',
+          appBar: AppBar(
+            backgroundColor: Colors.black87,
+            elevation: 5,
+            centerTitle: true,
+            title: Text(
+              'history',
+            ),
+            leading: IconButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  PageTransition(
+                      type: PageTransitionType.topToBottom, child: FirstPage()),
+                );
+              },
+              icon: Icon(Icons.keyboard_arrow_down),
+            ),
           ),
-          leading: IconButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                PageTransition(
-                    type: PageTransitionType.topToBottom, child: FirstPage()),
+          body: ListView.separated(
+            itemCount: 7,
+            itemBuilder: (BuildContext ctx, int idx) {
+              return Row(
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(top: 10, left: 10),
+                    height: 150,
+                    width: 150,
+                    child: Text('data number ${idx}'),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(left: 20),
+                    child: Text('data'),
+                  ),
+                ],
               );
             },
-            icon: Icon(Icons.keyboard_arrow_down),
-          ),
-        ),
-      ),
+            separatorBuilder: (BuildContext ctx, int idx) {
+              return Divider();
+            },
+          )),
     );
   }
 }
